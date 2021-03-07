e48a5a4c53f1ee7debc6c66a766bc8a873f6b53d
gerardine2001 <gerardinekaba@gmail.com>
2021-03-07 10:30:29 +0300

    assignment1

---
+++ assignment.js
@@ -0,0 +1,6 @@
+function fruits(){
+    var fruits_list=["kiwi","strawberry","gruvu","tomato","mango"];
+      fruits_list.forEach(Element => console.log(Element));
+      console.log(fruits_list[0]);
+  }
+  fruits();
